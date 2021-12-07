794019b6066a0cb5b87e87450cbcc7865f67e0a5
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-12-07 19:55:19 -0300

    Cria lógica quando clicar logo ROIT volta home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
 import '../css/Header.css';
 import ROIT from '../images/roit-logo.png';
 
 function Header() {
+  const [returnHome, setReturnHome] = useState(false);
+
   return (
     <header className="header-container">
       <img
@@ -11,7 +15,9 @@ function Header() {
         height="45px"
         width="130px"
         className="logo-image"
+        onClick={ () => setReturnHome(true) }
       />
+      { returnHome && <Navigate to="/home" /> }
     </header>
   )
 }
